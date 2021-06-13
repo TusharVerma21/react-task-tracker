a39084f3eb021a859c87ed8feb9cfa04d3492c1c
Tushar Verma <tusharverma.tv1999@gmail.com>
2021-06-13 19:32:23 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import './App.css';
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/Header";
